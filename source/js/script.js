8ecbde7e3215ea667867286e55671584df5d0444
reihaneh hosseini <reihaneh_h1368@yahoo.com>
2024-03-02 17:03:14 +0330

    do it

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+﻿let userName = document.querySelector(".username");
+let password = document.querySelector(".password");
+
+let warningUserName = document.querySelector(".warningusername");
+let warningPassword = document.querySelector(".warningpassword");
+
+userName.addEventListener("keypress", function () {
+  if (userName.value.length < 12) {
+    warningUserName.style.display = "inline";
+    warningUserName.style.color = "red";
+  }  if(userName.value.length = 0){
+    warningUserName.style.display = "none"
+  }else{
+    userName.style.display = "inline";
+    warningUserName.innerHTML = "its correct";
+    warningUserName.style.color = "green"
+  }});
